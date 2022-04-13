30650e223f68e9f7eb3b082ba3591b60f647b484
yatintri <yatintripathi0679@gmail.com>
2022-04-13 10:21:28 +0530

    added nothing

--- src/main/java/com/yatin/SpringBoottutorial/controller/CourseController.java
+++ src/main/java/com/yatin/SpringBoottutorial/controller/CourseController.java
@@ -14,6 +14,7 @@ import com.yatin.SpringBoottutorial.entity.Course;
 //import com.yatin.SpringBoottutorial.repository.CourseMaterialRepository;
 import com.yatin.SpringBoottutorial.service.CourseService;
 
+//this is controller
 @RestController
 public class CourseController {
 	@Autowired
