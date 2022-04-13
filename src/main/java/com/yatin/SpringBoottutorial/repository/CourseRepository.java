4d7ad05655b18d80a500c1f703512e351f9b9dee
yatintri <yatintripathi0679@gmail.com>
2022-04-13 10:39:42 +0530

    added

--- src/main/java/com/yatin/SpringBoottutorial/repository/CourseRepository.java
+++ src/main/java/com/yatin/SpringBoottutorial/repository/CourseRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import com.yatin.SpringBoottutorial.entity.Course;
 
+//done by yatin
 @Repository
 @Transactional
 public interface CourseRepository extends JpaRepository<Course, Long>{
